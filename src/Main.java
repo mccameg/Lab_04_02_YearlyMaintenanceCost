64197d68535fb2c99855ac43b111541cb68bd0b2
mccameg <meganmccarren@yahoo.com>
2023-02-05 21:24:22 -0500

    Final

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	double summerCost = 50;
 	double yearlyCost = 0;
 	yearlyCost = fallCost + winterCost + springCost + summerCost; // calculates the yearly cost by adding all the seasonal costs together
-	System.out.println("The seasonal costs are $" + fallCost + ", $" + winterCost + ", $" + springCost + ", $" + summerCost + ". " + "The total yearly maintenance cost is $" + yearlyCost);
+	System.out.println("The seasonal costs are $" + fallCost + ", $" + winterCost + ", $" + springCost + ", and $" + summerCost + ". " + "The total yearly maintenance cost is $" + yearlyCost);
 		// outputs the seasonal costs and the total yearly cost
     }
 }
